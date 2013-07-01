7446a450009e9e06da42cc73f1bbde74d75f6df4
Julian Mclean <julian.s.mclean@gmail.com>
2013-07-01 19:36:14 +0100

    First addition of mood and sequence support

--- lib/lwrf/lwrf-version.rb
+++ lib/lwrf/lwrf-version.rb
@@ -1,3 +1,3 @@
 class Lwrf
-	VERSION = "0.0.8"
+	VERSION = "0.0.9"
 end
--- lib/siriproxy-lwrf.rb
+++ lib/siriproxy-lwrf.rb
@@ -44,20 +44,29 @@ class SiriProxy::Plugin::Lwrf < SiriProxy::Plugin
 
   # Commands for Rooms and Devices
   LightWaveRF.new.get_config['room'].each do | room |
-    room["device"].each do | deviceName |
-    
+    room["device"].each do | deviceName |    
       # Commands to turn on/off a device in a room
-      listen_for (/turn (on|off) the (#{deviceName}) in the (#{room["name"]})/i) { |action, deviceName, roomName| send_lwrf_command(roomName,deviceName,action) }
-      listen_for (/turn (on|off) the (#{room["name"]}) (#{deviceName})/i)        { |action, roomName, deviceName| send_lwrf_command(roomName,deviceName,action) }
-      listen_for (/turn the (#{deviceName}) in the (#{room["name"]}) (on|off)/i) { |deviceName, roomName, action| send_lwrf_command(roomName,deviceName,action) }
-      listen_for (/turn the (#{room["name"]}) (#{deviceName}) (on|off)/i)        { |roomName, deviceName, action| send_lwrf_command(roomName,deviceName,action) }
+      listen_for (/turn (on|off) the (#{deviceName}) in the (#{room["name"]})/i) { |action, deviceName, roomName| send_lwrf_command('device',roomName,deviceName,action) }
+      listen_for (/turn (on|off) the (#{room["name"]}) (#{deviceName})/i)        { |action, roomName, deviceName| send_lwrf_command('device',roomName,deviceName,action) }
+      listen_for (/turn the (#{deviceName}) in the (#{room["name"]}) (on|off)/i) { |deviceName, roomName, action| send_lwrf_command('device',roomName,deviceName,action) }
+      listen_for (/turn the (#{room["name"]}) (#{deviceName}) (on|off)/i)        { |roomName, deviceName, action| send_lwrf_command('device',roomName,deviceName,action) }
 
       # Commands to dim a devices in a room
-      listen_for (/(?:(?:dim)|(?:set)|(?:turn up)|(?:turn down)|(?:set level on)|(?:set the level on)) the (#{deviceName}) in the (#{room["name"]}) to ([1-9][0-9]?)(?:%| percent)?/i) { |deviceName, roomName, action| send_lwrf_command(roomName,deviceName,action) }
-      listen_for (/(?:(?:dim)|(?:set)|(?:turn up)|(?:turn down)|(?:set level on)|(?:set the level on)) the (#{room["name"]}) (#{deviceName}) to ([1-9][0-9]?)(?:%| percent)?/i)        { |roomName, deviceName, action| send_lwrf_command(roomName,deviceName,action) }
-
+      listen_for (/(?:(?:dim)|(?:set)|(?:turn up)|(?:turn down)|(?:set level on)|(?:set the level on)) the (#{deviceName}) in the (#{room["name"]}) to ([1-9][0-9]?)(?:%| percent)?/i) { |deviceName, roomName, action| send_lwrf_command('device',roomName,deviceName,action) }
+      listen_for (/(?:(?:dim)|(?:set)|(?:turn up)|(?:turn down)|(?:set level on)|(?:set the level on)) the (#{room["name"]}) (#{deviceName}) to ([1-9][0-9]?)(?:%| percent)?/i)        { |roomName, deviceName, action| send_lwrf_command('device',roomName,deviceName,action) }
+    end
+    
+    room["mood"].each do | moodName |    
+      # Commands to set a mood in a room
+      listen_for (/(?:(?:set)|(?:activate)) (?:the )(#{moodName}) in the (#{room["name"]})/i) { |moodName, roomName| send_lwrf_command('mood',roomName,moodName) }
     end
   end
+  
+  LightWaveRF.new.get_config['sequence'].each do | config, sequenceName |
+    # Commands to run a sequence
+    listen_for (/(?:(?:run)|(?:launch)|(?:activate)) (?:the )(?:sequence )(#{sequenceName}) /i) { |sequenceName| send_lwrf_command('sequence',sequenceName) }    
+  end
+  
 
   # Commands to update config file
   listen_for (/(?:(?:update)|(?:download))(?: my)? lightwave (?:(?:config)|(?:setup)|(?:data)|(?:device list))/i) do 
@@ -71,33 +80,72 @@ class SiriProxy::Plugin::Lwrf < SiriProxy::Plugin
   end
   
 
-  def send_lwrf_command (roomName, deviceName, action)  
+  def send_lwrf_command (type, roomName, deviceName, action)  
     begin
-      @debug and (puts "[Info - Lwrf] send_lwrf_command: Starting with arguments: roomName => #{roomName}, deviceName => #{deviceName}, action => #{action} ")
+      @debug and (puts "[Info - Lwrf] send_lwrf_command: Starting with arguments: type => #{type}, roomName => #{roomName}, deviceName => #{deviceName}, action => #{action} ")
       # initialise LightWaveRF Gem
       @debug and (puts "[Info - Lwrf] send_lwrf_command: Instantiating LightWaveRF Gem")
       lwrf = LightWaveRF.new rescue nil
       @debug and (puts "[Info - Lwrf] send_lwrf_command: lwrf => #{lwrf}" )
       lwrfConfig = lwrf.get_config rescue nil
       @debug and (puts "[Info - Lwrf] send_lwrf_command: lwrfConfig => #{lwrfConfig}" )
+      
+      case type
 
-      # Validate Inputs - NB: siri passes lowercase values, but lwrf.send is case sensitive!
-      if lwrfConfig.has_key?("room")
-        room = lwrfConfig["room"].detect {|r| r["name"].downcase == roomName}
-        if room 
-          device = room["device"].detect {|d| d.downcase == deviceName} if room["device"]
-          if device
-            say "Turning #{action} the #{device} in the #{room["name"]}."
-            lwrf.send "#{room["name"]}", "#{device}", "#{action}", @debug rescue nil
-            @debug and (puts "[Info - Lwrf] send_lwrf_command: Command sent to LightWaveRF Gem" )
+      # if a mood...
+      when 'mood'
+        if lwrfConfig.has_key?("room")
+          room = lwrfConfig["room"].detect {|r| r["name"].downcase == roomName}
+          if room 
+            mood = room["mood"].detect {|d| d.downcase == deviceName} if room["mood"]
+            if mood
+              say "Setting mood #{mood} in the #{room["name"]}."
+              lwrf.mood "#{room["name"]}", "#{mood}", @debug rescue nil
+              @debug and (puts "[Info - Lwrf] send_lwrf_command: Command sent to LightWaveRF Gem" )
+            else
+              say "I'm sorry, I can't find a mood called '#{deviceName}' in the '#{roomName}'."
+            end
           else
-            say "I'm sorry, I can't find '#{deviceName}' in the '#{roomName}'."
+            say "I'm sorry, I can't find '#{roomName}'."
           end
-        else
-          say "I'm sorry, I can't find '#{roomName}'."
+        else    
+          say "I'm sorry, I can't find any rooms in your config file."
         end
-      else    
-        say "I'm sorry, I can't find either '#{roomName}' or '#{deviceName}'."
+        
+      # if a sequence...
+      when 'sequence'
+        if lwrfConfig.has_key?("sequence")
+          if lwrfConfig["sequence"].has_key?(roomName)
+              say "Running sequence #{roomName}."
+              lwrf.sequence "#{roomName}", @debug rescue nil
+              @debug and (puts "[Info - Lwrf] send_lwrf_command: Command sent to LightWaveRF Gem" )
+          else
+            say "I'm sorry, I can't find a sequence called '#{roomName}'."
+          end
+        else    
+          say "I'm sorry, I can't find any sequences in your config file."
+        end
+
+      # else a device
+      else
+        # Validate Inputs - NB: siri passes lowercase values, but lwrf.send is case sensitive!
+        if lwrfConfig.has_key?("room")
+          room = lwrfConfig["room"].detect {|r| r["name"].downcase == roomName}
+          if room 
+            device = room["device"].detect {|d| d.downcase == deviceName} if room["device"]
+            if device
+              say "Turning #{action} the #{device} in the #{room["name"]}."
+              lwrf.send "#{room["name"]}", "#{device}", "#{action}", @debug rescue nil
+              @debug and (puts "[Info - Lwrf] send_lwrf_command: Command sent to LightWaveRF Gem" )
+            else
+              say "I'm sorry, I can't find '#{deviceName}' in the '#{roomName}'."
+            end
+          else
+            say "I'm sorry, I can't find '#{roomName}'."
+          end
+        else    
+          say "I'm sorry, I can't find either '#{roomName}' or '#{deviceName}'."
+        end
       end
 
     rescue Exception
